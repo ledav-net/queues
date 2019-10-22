ba49e41de70e290ca2a3edac1e3a015ce55e25e2
David De Grave <david@ledav.net>
2019-10-22 22:26:06 +0200

    Add license...

--- queues.c
+++ queues.c
@@ -1,4 +1,18 @@
-
+/*
+ * Copyright (C) 2009 David De Grave <david@ledav.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
